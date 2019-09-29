b673a10f43b3318c41da061e39198e32e46b84cb
MuhammadKamran <muhammadkamran@qq.com>
2019-09-29 12:21:03 +0800

    Tackled mis-spelled input by user by using get_close_matches

--- english-dictionary.py
+++ english-dictionary.py
@@ -1,4 +1,5 @@
 import json
+from difflib import get_close_matches
 
 data = json.load(open("data.json"))
 
@@ -6,7 +7,11 @@ def translate (w):
     w = w.lower() #It will convert all input to lowercase
     #lowercase will help in tackling input mix case letters
     if w in data:
-        return [w]
+        return data[w]   #If match is found, it return the word
+    elif len(get_close_matches(w, data.keys())) > 0:   #If there are similar words, it will be returned
+        yesno = input("Did you mean %s instead? Enter Y if yes, or N if no." % get_close_matches(w, data.keys())[0])   #Using first similar word from aboe step
+        if yesno =="Y" or "y":
+            return data[get_close_matches(w, data.keys())[0]]
     else:
         return "The word does not exist. Please double check it"
 
