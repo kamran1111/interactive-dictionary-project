6400c3af8b05c83c631c8e50949706ff3f7007a7
MuhammadKamran <muhammadkamran@qq.com>
2019-09-29 12:29:32 +0800

    Uppercase or loser case Y or N entered by user.

--- english-dictionary.py
+++ english-dictionary.py
@@ -9,9 +9,14 @@ def translate (w):
     if w in data:
         return data[w]   #If match is found, it return the word
     elif len(get_close_matches(w, data.keys())) > 0:   #If there are similar words, it will be returned
-        yesno = input("Did you mean %s instead? Enter Y if yes, or N if no." % get_close_matches(w, data.keys())[0])   #Using first similar word from aboe step
-        if yesno =="Y" or "y":
+        yesno = input("Did you mean - %s - instead? Enter Y if yes, or N if no: " % get_close_matches(w, data.keys())[0])   #Using first similar word from aboe step
+        yesno = yesno.upper()  #capital or small case Yes [y] or No [N}
+        if yesno == "Y":
             return data[get_close_matches(w, data.keys())[0]]
+        elif yesno == "N":
+            return "The word does not exist. Please double check it"
+        else:
+            return "We didn't understand the word you entered"
     else:
         return "The word does not exist. Please double check it"
 
